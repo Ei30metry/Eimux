c66e047a5931c464a2e8b70cf7521f4e834d8f54
Artin Ghasivand <ghasivand.artin@gmail.com>
2025-03-25 17:28:54 +0330

    Get rid of perspective

--- lisp/window-frame-config.el
+++ lisp/window-frame-config.el
@@ -3,20 +3,38 @@
 (setq confirm-kill-emacs 'y-or-n-p)
 (set-frame-parameter nil 'internal-border-width 0)
 
-(use-package perspective
-  :straight t
-  :custom
-  (persp-mode-prefix-key (kbd "C-x C-,"))
+(use-package tab-bar-mode
   :bind
-  ("C-x k" . (lambda () (interactive) (persp-kill-buffer* nil)))
-  ("C-x C-, C-," . persp-switch)
-  ("C-x C-, C-." . persp-switch-last)
-  ("C-x K" . persp-kill-buffer*)
-  ("C-." . persp-switch-to-buffer*)
-  ("C-x b" . switch-to-buffer)
-  :init
-  (setq persp-initial-frame-name "misc")
-  (persp-mode))
+  ("C-x C-, C-," . tab-switch)
+  ("C-x C-, C-k" . tab-close)
+  ("C-x C-, C-K" . tab-close-other)
+
+  ("C-x C-, C-n" . tab-next)
+  ("s-]"         . tab-next)
+
+  ("C-x C-, C-p" . tab-previous)
+  ("s-["         . tab-previous)
+  :config
+  (tab-bar-mode 1)
+  (setq tab-bar-show nil))
+
+(global-set-key (kbd "C-x b") 'consult-buffer)
+(global-set-key (kbd "C-.") 'consult-buffer)
+
+;; (use-package perspective
+;;   :straight t
+;;   :custom
+;;   (persp-mode-prefix-key (kbd "C-x C-,"))
+;;   :bind
+;;   ("C-x k" . (lambda () (interactive) (persp-kill-buffer* nil)))
+;;   ("C-x C-, C-," . persp-switch)
+;;   ("C-x C-, C-." . persp-switch-last)
+;;   ("C-x K" . persp-kill-buffer*)
+;;   ("C-." . persp-switch-to-buffer*)
+;;   ("C-x b" . switch-to-buffer)
+;;   :init
+;;   (setq persp-initial-frame-name "misc")
+;;   (persp-mode))
 
 (global-set-key (kbd "C-x w m") 'maximize-window)
 (global-set-key (kbd "C-x w u") 'winner-undo)
@@ -82,6 +100,8 @@
 
 (use-package ligature :straight t)
 
+(use-package show-font :straigh t)
+
 (defun ask-before-closing ()
   "Close only if y was pressed."
   (interactive)
@@ -101,5 +121,15 @@
         :bind
         ("C-x o" . other-window)
         ("M-o" . ace-window))
+
+(use-package ultra-scroll
+  :straight
+  (ultra-scroll
+   :type git
+   :host github
+   :repo "jdtsmith/ultra-scroll"
+   :files ("*.el"))
+  :config
+  (ultra-scroll-mode 1))
 
 (provide 'window-frame-config)
